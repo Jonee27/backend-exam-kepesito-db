f187f952ce4ed1ae3261a950eb0d195aef0aeb5e
Jonee27 <jonee271@gmail.com>
2021-04-29 10:43:44 +0200

    Done maybe??

--- src/main/java/hu/nive/ujratervezes/jurassic/JurassicPark.java
+++ src/main/java/hu/nive/ujratervezes/jurassic/JurassicPark.java
@@ -1,5 +1,10 @@
 package hu.nive.ujratervezes.jurassic;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 public class JurassicPark {
 
     private String dbUrl;
@@ -10,6 +15,26 @@ public class JurassicPark {
         this.dbUrl = dbUrl;
         this.dbUser = dbUser;
         this.dbPassword = dbPassword;
+    }
+
+    private Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+    }
+
+    public List<String> checkOverpopulation() {
+        List<String> dinosaurs = new ArrayList<>();
+        String sql = "SELECT breed FROM dinosaur WHERE expected < actual ORDER BY breed";
+
+        try (Connection connection = getConnection(); PreparedStatement pstm = connection.prepareStatement(sql)) {
+            ResultSet resultSet = pstm.executeQuery();
+            while (resultSet.next()) {
+                dinosaurs.add(resultSet.getString(1));
+            }
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return dinosaurs;
     }
 
 }
